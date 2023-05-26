fcf654e1fa60fa1b145042da0f013cff0d4952c4
Zoeyzyy <1162651439@qq.com>
2023-05-26 14:02:19 +0800

    merge

---
+++ pluginwidget.cpp
@@ -0,0 +1,51 @@
+#include "pluginwidget.h"
+#include "dde-dock/constants.h"
+#include <QApplication>
+#include <QPainter>
+#include <QDebug>
+#include <QMouseEvent>
+
+#define PLUGIN_STATE_KEY    "enable"
+
+PluginWidget::PluginWidget(QWidget *parent)
+    : QWidget(parent)
+{
+    text = "SELF-STARTUP";
+    // mp = 0;
+    // cp = 0;
+}
+
+bool PluginWidget::enabled()
+{
+    return m_settings.value(PLUGIN_STATE_KEY, true).toBool();
+}
+
+void PluginWidget::setEnabled(const bool b)
+{
+    m_settings.setValue(PLUGIN_STATE_KEY, b);
+}
+
+QSize PluginWidget::sizeHint() const
+{
+    QFont font = qApp->font();
+    font.setFamily("Noto Mono");
+    QFontMetrics FM(font);
+    return FM.boundingRect(" ↑0.00KB/s ").size() + QSize(0, FM.boundingRect(" ↓0.00KB/s ").height());
+}
+
+void PluginWidget::resizeEvent(QResizeEvent *e)
+{
+    QWidget::resizeEvent(e);
+}
+
+void PluginWidget::paintEvent(QPaintEvent *e)
+{
+    Q_UNUSED(e);
+    QPainter painter(this);
+    painter.setRenderHint(QPainter::Antialiasing);
+    painter.setPen(Qt::white);
+    QFont font = qApp->font();
+    font.setFamily("Noto Mono");
+    painter.setFont(font);
+    painter.drawText(rect().adjusted(2,0,0,0), Qt::AlignLeft | Qt::AlignVCenter, text);
+}
---
+++ pluginwidget.h
@@ -0,0 +1,32 @@
+#ifndef PLUGINWIDGET_H
+#define PLUGINWIDGET_H
+
+#include <QWidget>
+#include <QSettings>
+
+class PluginWidget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit PluginWidget(QWidget *parent = 0);
+    bool enabled();
+    void setEnabled(const bool b);
+    QString text;
+    // int mp, cp;
+
+// signals:
+//     void requestUpdateGeometry() const;
+
+private:
+    QSize sizeHint() const;
+    void resizeEvent(QResizeEvent *e);
+    void paintEvent(QPaintEvent *e);
+    QPixmap m_cachedIcon;
+    QString m_cachedTime;
+    QSettings m_settings;
+    bool m_24HourFormat;
+
+};
+
+#endif // PLUGINWIDGET_H
