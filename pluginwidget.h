c217eae9f00fda4faa6dc4544292c8c05b96f5d3
Zoeyzyy <1162651439@qq.com>
2023-05-26 14:37:12 +0800

    change pluginwidget

--- pluginwidget.h
+++ pluginwidget.h
@@ -2,27 +2,30 @@
 #define PLUGINWIDGET_H
 
 #include <QWidget>
-#include <QSettings>
+#include <QLabel>
+#include <QTimer>
+#include <QStorageInfo>
 
 class PluginWidget : public QWidget
 {
     Q_OBJECT
 
 public:
-    explicit PluginWidget(QWidget *parent = 0);
-    // bool enabled();
-    // void setEnabled(const bool b);
-    QString text;
-    // int mp, cp;
+    explicit PluginWidget(QWidget *parent = nullptr);
 
-// signals:
-//     void requestUpdateGeometry() const;
+    inline QStorageInfo * storageInfo() { return m_storageInfo; }
+
+private slots:
+    // 用于更新数据的槽函数
+    void refreshInfo();
 
 private:
-    QSize sizeHint() const;
-    void resizeEvent(QResizeEvent *e);
-    void paintEvent(QPaintEvent *e);
-
+    // 真正的数据显示在这个 Label 上
+    QLabel *m_infoLabel;
+    // 处理时间间隔的计时器
+    QTimer *m_refreshTimer;
+    // 分区数据的来源
+    QStorageInfo *m_storageInfo;
 };
 
 #endif // PLUGINWIDGET_H
